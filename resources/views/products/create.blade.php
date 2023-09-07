74f0417b6b6def51594b42af05a60bf61a4d6a76
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-09-07 06:01:20 +0100

    updated

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,10 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Product Form</title>
 </head>
 <body>
-    <h1>Create a Product</h1>
-    <div>index</div>
+    <h2>Add a Product</h2>
+    <form action="process_form.php" method="POST">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
+        
+        <label for="quantity">Quantity:</label>
+        <input type="number" id="quantity" name="quantity" required><br><br>
+        
+        <label for="price">Price:</label>
+        <input type="number" step="0.01" id="price" name="price" required><br><br>
+        
+        <label for="description">Description:</label><br>
+        <textarea id="description" name="description" rows="4" cols="50" required></textarea><br><br>
+        
+        <input type="submit" value="save">
+    </form>
 </body>
 </html>
