9ef15d17ef3acd1e841c41f14145f593b634b8df
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-09-11 14:56:19 +0100

    updated

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,14 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Products</h1>
     <div>
-    
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Qty</th>
+                <th>Price</th>
+                <th>Description</th>
+            </tr>
+
+            @foreach ($products as $product)
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->name}}</td>
+                    <td>{{$product->quantity}}</td>
+                    <td>{{$product->price}}</td>
+                    <td>{{$product->description}}</td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 </body>
+
 </html>
